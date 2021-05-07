fede9672e8000611578c976c6086e54ca0545231
JulienRZ-Web <rz.web.development@gmail.com>
2021-05-07 11:05:19 +0200

    added the ESP LRP LRPT EST_SPT

--- src/main/java/jobshop/MainTest.java
+++ src/main/java/jobshop/MainTest.java
@@ -4,6 +4,7 @@ import jobshop.encodings.JobNumbers;
 import jobshop.encodings.ResourceOrder;
 import jobshop.encodings.Schedule;
 import jobshop.encodings.Task;
+import jobshop.solvers.GreedySolver;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -14,8 +15,9 @@ public class MainTest {
     public static void main(String[] args) {
         try {
             // load the aaa1 instance
-            Instance instance = Instance.fromFile(Paths.get("instances/aaa1"));
+            Instance instance = Instance.fromFile(Paths.get("instances/aaa3"));
 
+            /*
             // builds a solution in the job-numbers encoding [0 0 1 1 0 1]
             JobNumbers enc = new JobNumbers(instance);
             enc.addTaskOfJob(0);
@@ -33,14 +35,74 @@ public class MainTest {
             System.out.println("MAKESPAN: " + schedule.makespan());
             System.out.println("SCHEDULE: " + schedule.toString());
             System.out.println("GANTT: " + schedule.asciiGantt());
+            */
+
+
+            GreedySolver solver = new GreedySolver(GreedySolver.Priority.EST_LRPT);
+
+            Result result = solver.solve(instance, 1000);
+
+            System.out.println("Solution found by the greedy solver :");
+            System.out.println("MAKESPAN: " + result.schedule.get().makespan());
+            System.out.println(result.schedule.get().asciiGantt());
+
+            /*
+            // MANUAL SCHEDULE
 
             Schedule manualSchedule = new Schedule(instance);
-            // TODO: encode the same solution
-            //manualSchedule.setStartTime(....);
+
+            manualSchedule.setStartTime(0, 0, 0);
+            manualSchedule.setStartTime(0, 1, 3);
+            manualSchedule.setStartTime(0, 2, 6);
+            manualSchedule.setStartTime(1, 0, 6);
+            manualSchedule.setStartTime(1, 1, 8);
+            manualSchedule.setStartTime(1, 2, 10);
+
+            System.out.println("*****MANUAL SCHEDULE*****");
+            System.out.println(manualSchedule.asciiGantt());
+
+
+            // MANUAL RESOURCE ODER
 
             ResourceOrder manualRO = new ResourceOrder(instance);
-            // TODO: encode the same solution
-            //manualRO.addTaskToMachine(..., new Task(..., ...));
+
+            manualRO.addTaskToMachine(0, new Task(0, 0));
+            manualRO.addTaskToMachine(0, new Task(1, 1));
+            manualRO.addTaskToMachine(1, new Task(0, 1));
+            manualRO.addTaskToMachine(1, new Task(1, 0));
+            manualRO.addTaskToMachine(2, new Task(0, 2));
+            manualRO.addTaskToMachine(2, new Task(1, 2));
+
+            System.out.println("*****MANUAL RO*****");
+            System.out.println(manualRO.toSchedule().get().asciiGantt());
+
+
+            ResourceOrder optimalRO = new ResourceOrder(instance);
+
+            optimalRO.addTaskToMachine(0, new Task(0, 0));
+            optimalRO.addTaskToMachine(0, new Task(1, 1));
+            optimalRO.addTaskToMachine(1, new Task(1, 0));
+            optimalRO.addTaskToMachine(1, new Task(0, 1));
+            optimalRO.addTaskToMachine(2, new Task(1, 2));
+            optimalRO.addTaskToMachine(2, new Task(0, 2));
+
+
+            System.out.println("*****OPTIMAL RO*****");
+            System.out.println(optimalRO.toSchedule().get().asciiGantt());
+            /*
+            ResourceOrder errorRO = new ResourceOrder(instance);
+
+            errorRO.addTaskToMachine(0, new Task(1, 1));
+            errorRO.addTaskToMachine(0, new Task(0, 0));
+            errorRO.addTaskToMachine(1, new Task(0, 1));
+            errorRO.addTaskToMachine(1, new Task(1, 0));
+            errorRO.addTaskToMachine(2, new Task(1, 2));
+            errorRO.addTaskToMachine(2, new Task(0, 2));
+
+
+            System.out.println("*****ERROR RO*****");
+            System.out.println(errorRO.toSchedule().get().asciiGantt());
+             */
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/jobshop/solvers/GreedySolver.java
+++ src/main/java/jobshop/solvers/GreedySolver.java
@@ -2,6 +2,10 @@ package jobshop.solvers;
 
 import jobshop.Instance;
 import jobshop.Result;
+import jobshop.encodings.ResourceOrder;
+import jobshop.encodings.Task;
+
+import java.util.ArrayList;
 
 /** An empty shell to implement a greedy solver. */
 public class GreedySolver implements Solver {
@@ -19,8 +23,189 @@ public class GreedySolver implements Solver {
         this.priority = p;
     }
 
+    // Handle start times memory for EST
+    private ArrayList<Integer> jobsDispoTimes = new ArrayList<Integer>();
+    private ArrayList<Integer> machinesDispoTimes =  new ArrayList<Integer>();
+
+
     @Override
     public Result solve(Instance instance, long deadline) {
-        throw new UnsupportedOperationException();
+
+        ResourceOrder ro = new ResourceOrder(instance);
+        ArrayList<Task> possibleTasks = new ArrayList<Task>();
+
+        // init jobsDispoTime
+        for(int i = 0; i < instance.numJobs; i++) {
+            jobsDispoTimes.add(0);
+        }
+
+        // init machinesDispoTimes
+        for(int i = 0; i < instance.numMachines; i++) {
+            machinesDispoTimes.add(0);
+        }
+
+        // first possible tasks
+        for(int i = 0; i < instance.numJobs; i++) {
+            Task task = new Task(i, 0);
+            possibleTasks.add(task);
+        }
+
+        // Loop while there is tasks to do
+        while(!possibleTasks.isEmpty()) {
+
+            Task task = null; // the task that will be added to the ro
+
+            // what kind of solver do we want to use ?
+            switch(this.priority) {
+                case SPT:
+                    // Return the shortest possible task
+                    task = getTaskSTP(instance, possibleTasks);
+                    break;
+                case LPT:
+                    // Return the longest possible task
+                    task = getTaskLPT(instance, possibleTasks);
+                    break;
+                case LRPT:
+                    // Return the longest possible task
+                    task = getTaskLRPT(instance, possibleTasks);
+                    break;
+                case EST_SPT:
+                    // Return the shortest possible task that can start earlier
+                    task = getTaskEST_SPT(instance, possibleTasks);
+                    jobsDispoTimes.set(task.job, instance.duration(task));
+                    machinesDispoTimes.set(instance.machine(task), instance.duration(task));
+                    break;
+                case EST_LRPT:
+                    // Return the longest remaining process possible task that can start earlier
+                    task = getTaskEST_LRPT(instance, possibleTasks);
+                    jobsDispoTimes.set(task.job, instance.duration(task));
+                    machinesDispoTimes.set(instance.machine(task), instance.duration(task));
+
+                    System.out.println("possibles tasks : ");
+                    for(Task t : possibleTasks) {
+                        System.out.println(t.toString());
+                    }
+
+                    System.out.println("chosen task : ");
+                    System.out.println(task);
+                    break;
+                default:
+                    // Return the shortest possible task
+                    task = getTaskSTP(instance, possibleTasks);
+                    break;
+            }
+
+            // Add the task to the ro, remove the task from the possibles tasks
+            ro.addTaskToMachine(instance.machine(task), task);
+            possibleTasks.remove(task);
+
+            if(task.task < instance.numTasks - 1) {
+                possibleTasks.add(new Task(task.job, task.task + 1));
+            }
+        }
+
+        // return the result
+        return new Result(instance, ro.toSchedule(), Result.ExitCause.ProvedOptimal);
+    }
+
+
+    // STP : shortest processing task
+    private Task getTaskSTP(Instance instance, ArrayList<Task> tasks) {
+        Task minTask = tasks.get(0);
+        int minDuration = Integer.MAX_VALUE;
+
+        for(Task task : tasks) {
+            if(instance.duration(task) < minDuration) {
+                minTask = task;
+                minDuration = instance.duration(task);
+            }
+        }
+
+        return minTask;
+    }
+
+    // LPT : longest processing task
+    private Task getTaskLPT(Instance instance, ArrayList<Task> tasks) {
+        Task maxTask = tasks.get(0);
+        int maxDuration = Integer.MIN_VALUE;
+
+        for(Task task : tasks) {
+            if(instance.duration(task) > maxDuration) {
+                maxTask = task;
+                maxDuration = instance.duration(task);
+            }
+        }
+
+        return maxTask;
+    }
+
+    // LRPT : Longest remaining processing task
+    private Task getTaskLRPT(Instance instance, ArrayList<Task> tasks) {
+        Task LRPTTask = tasks.get(0);
+        int maxJob = Integer.MIN_VALUE;
+
+        for(Task task : tasks) {
+            int remainingTime = getRemainingTimeInJob(instance, task.job, task.task);
+            if(remainingTime > maxJob) {
+                LRPTTask = task;
+                maxJob = remainingTime;
+            }
+        }
+
+        return LRPTTask;
+    }
+
+    private int getRemainingTimeInJob(Instance instance, int job, int task) {
+        int t = 0;
+        for(int i = task; i < instance.numTasks; i++) {
+            t += instance.duration(new Task(job, i));
+        }
+        return t;
+    }
+
+
+    // EST_SPT : shortest task that can start earlier
+    private Task getTaskEST_SPT(Instance instance, ArrayList<Task> tasks) {
+        ArrayList<Task> EST_SPTTasks = new ArrayList<Task>();
+        Task EST_SPTTask = null;
+        int earlierTask = Integer.MAX_VALUE;
+
+        for(Task task : tasks) {
+            int start_time = Math.max(jobsDispoTimes.get(task.job), machinesDispoTimes.get(instance.machine(task)));
+            if(start_time < earlierTask) {
+                EST_SPTTasks = new ArrayList<Task>();
+                EST_SPTTasks.add(task);
+                earlierTask = start_time;
+            } else if(start_time == earlierTask) {
+                EST_SPTTasks.add(task);
+            }
+        }
+
+        EST_SPTTask = getTaskSTP(instance, EST_SPTTasks);
+
+        return EST_SPTTask;
+    }
+
+    // EST_LRPT : longest remaining process task that can start earlier
+    private Task getTaskEST_LRPT(Instance instance, ArrayList<Task> tasks) {
+
+        ArrayList<Task> EST_LRPTTasks = new ArrayList<Task>();
+        Task EST_LRPTTask = null;
+        int earlierTask = Integer.MAX_VALUE;
+
+        for(Task task : tasks) {
+            int start_time = Math.max(jobsDispoTimes.get(task.job), machinesDispoTimes.get(instance.machine(task)));
+            if(start_time < earlierTask) {
+                EST_LRPTTasks = new ArrayList<Task>();
+                EST_LRPTTasks.add(task);
+                earlierTask = start_time;
+            } else if(start_time == earlierTask) {
+                EST_LRPTTasks.add(task);
+            }
+        }
+
+        EST_LRPTTask = getTaskLRPT(instance, EST_LRPTTasks);
+
+        return EST_LRPTTask;
     }
 }
